4c2215a512f0c9c1e1cfd7e4b649d40bd9f0f87d
Zhenzhong Gao <zhenzhong.gao@alumni.imperial.ac.uk>
2022-04-05 15:41:20 +0900

    Unfinished at present

--- LinearProgrammingTechniques/ConvexHullRegion.py
+++ LinearProgrammingTechniques/ConvexHullRegion.py
@@ -6,7 +6,31 @@ __author__ = 'Zhenzhong Gao at Inuiguchi Laboratory'
 import numpy as np
 from sci.spatial import ConvexHull
 
-def ConvexHullRegion(A, b):
+def ConvexHullRegion(A, b, **kw):
+    # Check the dimension of the inequality constraints region
+    m, n = A.shape
+    if b.shape != [m,1]:
+        raise ValueError('The size of the constraints is not correct')
+
+    # Define the center to generate a list of vertices
+    if 'center' not in kw:
+        center = np.ones(n)
+    else:
+        center = kw[center]
+
+    # Define the number of vertices to define the convex hull region
+    if 'number' not in kw:
+        number = 50
+    else:
+        number = kw[number]
+
+    # Generate a list of vertices around the center
+    rng = np.random.default_rng(8823)
+    points = rng.random((3 * number, n))
+    scale = np.linalg.norm(center)
+    points = scale * 2 * (points - 0.5)
+
+    # remove the vertices not in the region
 
 
 
@@ -15,15 +39,15 @@ if __name__ == '__main__':
     The main process to generate a convex hull region inside another
     region defined by a series of linear constraints.
 
-    Input:  A --> np.matrix with R(m * n)
-            b --> np.array with R(m) or np.matrix with R(m * 1)
-    Output: D --> np.matrix with R(l * p)
+    Input:  A --> np.array with R(m * n)
+            b --> np.array with R(m)
+    Output: D --> np.array with R(l * p)
             d --> np.array with R(p)
 
     The input variables forms a region constrained by a series of constraints
     with the following inequalities:
 
-        {x in R(n): A @ x <= b}.
+        {x in R(n)matrix matrix : A @ x <= b}.
 
     The output variables also forms a region constrained by a series of 
     constraints with the following inquealities:
